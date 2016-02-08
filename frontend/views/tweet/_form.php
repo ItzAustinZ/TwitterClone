b02673a78a432ce2b61ae3d97f9f009e53edbfe6
Bryant Jackson <bjackson_seller@outlook.com>
2016-02-08 15:17:53 -0500

    Finished tweet form. Made it look acceptable. closes #9

--- frontend/views/tweet/_form.php
+++ frontend/views/tweet/_form.php
@@ -3,23 +3,39 @@
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 
+use app\models\Tweet;
+
 /* @var $this yii\web\View */
 /* @var $model app\models\Tweet */
 /* @var $form yii\widgets\ActiveForm */
 ?>
 
-<div class="tweet-form">
-
-    <?php $form = ActiveForm::begin(); ?>
-
-    <?= $form->field($model, 'key')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'text')->textarea(['rows' => 6]) ?>
-
-    <div class="form-group">
-        <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
-    </div>
-
-    <?php ActiveForm::end(); ?>
-
+<div class="col-md-6 well">
+<?php
+    $form = ActiveForm::begin(['action' => '../tweet/create']);
+    echo "<div class='row-fluid'>"; 
+        echo "<div class='col-md-3'>";
+            echo Yii::$app->user->identity->username;
+            echo "<br/>";
+            $hash = hash("md5", Yii::$app->user->identity->username);
+            echo "<img src='http://www.gravatar.com/avatar/$hash?d=identicon' />";
+        echo "</div>";
+        echo "<div class='col-md-9 text-center'>";
+            echo "<div class='panel panel-default'>";
+                echo "<div class='panel-body'>";
+                    echo $form->field($model, 'key')->textInput(['maxlength' => true]);
+                    echo $form->field($model, 'text')->textarea(['rows' => 6]);
+                echo "</div>";
+            echo "</div>";
+        echo "</div>";
+    echo "</div>";
+    
+    echo "<div class='row-fluid'>";
+        echo "<div class='col-md-12'>";
+            echo "OPTIONS GO HERE";
+            echo Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']);
+        echo "</div>";
+    echo "</div>";
+    ActiveForm::end();
+?>
 </div>
--- frontend/views/tweet/gridview.php
+++ frontend/views/tweet/gridview.php
@@ -2,17 +2,29 @@
 /*
  * Pass an array of Tweet models to $tweets.
  */
+use app\models\Tweet;
 ?>
         
 <div class="body-content">
 <?php
 $i = 0;
+$renderedForm = false;
 foreach($tweets as $message)
 {
     //Open a new row.
     if($i == 0)
     {
-        echo "<div class='row-fluid'>";
+        echo "<div class='row'>";
+    }
+    if(!$renderedForm)
+    {
+        if(!isset($model))
+        {
+            $model = new Tweet();
+        }
+        $renderedForm = true;
+        $i++;
+        echo $this->render('_form', ['model' => $model,]);
     }
     echo $this->render('_cell', [
         'model' => $message,
