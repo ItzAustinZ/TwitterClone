798865b6b2d9b98247cbe1d1f65b2b7ea658e714
Bryant Jackson <bjackson_seller@outlook.com>
2016-02-12 13:49:31 -0500

    Index now redirects to /tweet/view-all fixes #17

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -121,7 +121,7 @@ class SiteController extends Controller
      */
     public function actionIndex()
     {
-        return $this->render('index');
+        return $this->redirect('./tweet/view-all');
     }
 
     /**
